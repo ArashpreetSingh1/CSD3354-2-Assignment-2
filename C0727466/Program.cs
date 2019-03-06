913c86ea5da67d8cc00530507604b4a526c721d3
visualstudio <visualstudio@122-PC24>
2019-03-06 10:23:38 -0500

    LAB 01

--- C0727466/Program.cs
+++ C0727466/Program.cs
@@ -4,12 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Student Name:- Arashpreet Singh Student Id:-C0727466
+//student Name:-Navjot Singh Gill Student Id:- C0726409
+//2019W CSD 3354 2
+//Assignment 2
+//March 6-2019
+
 namespace C0727466
 {
-    class Program
+    class proram
     {
         static void Main(string[] args)
         {
+            DelegateExercises a = new DelegateExercises();
+            a.Method2();
         }
     }
-}
+            public class DelegateExercises
+            {
+                public delegate void MyDelegate();
+
+                void Method1()
+                {
+                    Console.WriteLine("Method1");
+                    Console.ReadLine();
+
+                }
+                public void Method2()
+                {
+                    MyDelegate myDelegate = new MyDelegate(Method1);
+                    myDelegate();
+                }
+            }
+        }
+
+
+
+
